340c2b79bcbc6c78f286f972ed656030b79757bf
LucasBrito <lucasroger.mb@hotmail.com>
2023-06-17 21:08:34 -0300

    banco

--- src/loaders/mongodb.js
+++ src/loaders/mongodb.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 async function startDB() {
-    await mongoose.connect('mongodb+srv://lucasrogermb:projetotcc123@cluster0.0z8bkig.mongodb.net/test');
+    await mongoose.connect('mongodb+srv://lucasrogermb:projetotcc123@cluster0.0z8bkig.mongodb.net/teste1');
 };
 
 module.exports = startDB;
